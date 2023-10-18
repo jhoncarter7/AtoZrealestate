b91e673f8ef08fd6e32f5129b99fd7de3b03a1f2
jhoncarter7 <rainikhil044@gmail.com>
2023-10-19 00:59:12 +0530

    [feat]: modify

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -4,20 +4,20 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema({
     userName:{
         type: String,
-        require: true,
+        required: true,
         unique: true,
     },
     userEmail:{
         type: String,
-        require: true,
+        required: true,
         unique: true,
     },
     userPassword:{
         type: String,
-        require: true,
+        required: true,
        
     },
-})
+},{timestamps: true })
 
 const User = mongoose.model('User', userSchema)
 export default User;
