2de3c381a9ed0ad457dc3ad864216865b42132ed
jhoncarter7 <rainikhil044@gmail.com>
2023-10-21 19:14:28 +0530

    create middleware and function to handle possible  err

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,14 +1,14 @@
 import User from "../models/user.model.js";
-import bcrypt from 'bcrypt'
-export const signUp = async(req, res)=>{
-   const {userName, Email, Password} = req.body;
-   console.log("all details", userName, Email, Password)
-   const hashPassword =  bcrypt.hashSync(Password, 10)
-   const newUser = new User({userName, Email, Password: hashPassword})
-   try {
-    await newUser.save()
-    res.status(201).json("User Created Successfully")
-   } catch (error) {
-   res.status(500).json(error.messag)
-   }
-}
+import bcrypt from "bcrypt";
+export const signUp = async (req, res, next) => {
+  const { userName, Email, Password } = req.body;
+  console.log("all details", userName, Email, Password);
+  const hashPassword = bcrypt.hashSync(Password, 10);
+  const newUser = new User({ userName, Email, Password: hashPassword });
+  try {
+    await newUser.save();
+    res.status(201).json("User Created Successfully");
+  } catch (error) {
+    next(error);
+  }
+};
--- api/index.js
+++ api/index.js
@@ -1,25 +1,34 @@
-import express  from "express";
+import express from "express";
 import mongoose from "mongoose";
-import dotenv from "dotenv";  
-import getUser from './routes/user.routes.js'  
-import authRouter from './routes/auth.routes.js'
+import dotenv from "dotenv";
+import getUser from "./routes/user.routes.js";
+import authRouter from "./routes/auth.routes.js";
 // import User from "./models/user.model.js";
-dotenv.config();    
+dotenv.config();
 
-
-mongoose.connect(process.env.MONGODB_URI).then(()=>{
-    console.log('Connected to MongoDB')
-}).catch((err)=>{    
-    console.log(err)
-})  
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 const app = express();
-app.use(express.json())
+app.use(express.json());
 
-
-app.listen(3000, ()=>{
-    console.log('Server running on port 3000:')
-})
+app.listen(3000, () => {
+  console.log("Server running on port 3000:");
+});
 // here we first say that we are going to api than / categaries like auth, user than inside the categaries we target different route inthat categaries i.e /api/categaries(auth)/subcategaries(getuser, getdetails)
-app.use("/api/user", getUser)
-app.use("/api/auth", authRouter)
-
+app.use("/api/user", getUser);
+app.use("/api/auth", authRouter);
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error!";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
