c309bfb8b80e61460dc8e2131dc13d748f74b99c
jhoncarter7 <rainikhil044@gmail.com>
2023-10-25 20:03:06 +0530

    add redux persist

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { Provider } from "react-redux";
-import { store } from "./redux/store";
+import { persistor, store } from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
+    <PersistGate loading= {false} persistor={persistor} >
     <App />
+    </PersistGate>
   </Provider>
 );
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,8 +1,26 @@
-import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import { combineReducers, configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import userReducer from "./user/userSlice";
+import {persistReducer, persistStore} from 'redux-persist'
+import storage from "redux-persist/lib/storage";
+
+// combine use to combine more than one reducer in single rootReducer
+const rootReducer = combineReducers({user: userReducer})
+
+// persist config store all the rootReducer in localStorage with the name root
+const persistConfig = {
+    key: 'root',
+    storage,
+    version: 1
+}
+// this reducer target the value and connect it with config
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+// now we replace the reducer with persistedReducer
 export const store = configureStore({
-    reducer:{user: userReducer},
+    reducer: persistedReducer,
     middleware: (getDefaultMiddleware) => getDefaultMiddleware({
    serializableCheck: false
     })
 })
+// now we use persistor to make store globally available
+export const persistor = persistStore(store)
